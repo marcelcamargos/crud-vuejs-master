61920ca71668328e0ad6e14c1574f670e7248024
marcelcamargos <marcel.camargos@gmail.com>
2017-05-06 23:42:23 -0300

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <Tarefas></Tarefas>
+  </div>
+</template>
+
+<script>
+  import Tarefas from './Tarefas.vue'
+  export default {
+    components: {
+      Tarefas
+    }
+  }
+</script>
+<style>
+  body {
+    margin-top: 50px
+  }
+</style>
